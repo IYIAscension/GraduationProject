561b0df283fb3ce5ee795fb675cf36a492a1a3b4
Rasjaad <rasjaad@nivero.io>
2021-06-27 16:36:58 +0200

    remove loop

--- hierarchical-clustering/main_no_distance.py
+++ hierarchical-clustering/main_no_distance.py
@@ -133,61 +133,61 @@ if __name__ == "__main__":
                 'commons-cli', 'google-auto-value', 'gson', 'commons-io', 'commons-text', 'commonc-codec', ]
     projects1 = ['google-auto-common']
     reductions = [0.25, 0.5, 0.75]
-    for counter in range(30):
-        results_df = pandas.DataFrame(columns=['project', 'reduction', 'score', 'acc_avg', 'acc_min', 'acc_max'])
-        seed = random.randint(0, 99999)
-        for project in projects:
-            csvPath = directory + "/" + project
-            csvFile = Path(csvPath + "/clustering/characteristics.csv")
+    results_df = pandas.DataFrame(columns=['project', 'reduction', 'score', 'acc_avg', 'acc_min', 'acc_max'])
+    seed = random.randint(0, 99999)
+    for project in projects:
+        csvPath = directory + "/" + project
+        csvFile = Path(csvPath + "/clustering/characteristics.csv")
 
-            if not csvFile.is_file():
-                print("characteristics not found: " + project)
-                continue
+        if not csvFile.is_file():
+            print("characteristics not found: " + project)
+            continue
 
-            for reduction in reductions:
+        for reduction in reductions:
 
-                data = merge_csv_files(csvPath)
+            data = merge_csv_files(csvPath)
 
-                # define ordinal encoding
-                encoder = LabelEncoder()
-                data = data[["id", "mutOperator", "opcode", "returnType",
-                             "localVarsCount", "isInTryCatch", "isInFinalBlock", "className", "methodName",
-                             "blockNumber", "lineNumber", "numTests"]]
-                # Transform each column.. do id last since we need to inverse that.
-                for col in ["mutOperator", "returnType", "className", "methodName", "id"]:
-                    data[col] = encoder.fit_transform(data[col])
+            # define ordinal encoding
+            encoder = LabelEncoder()
+            data = data[["id", "mutOperator", "opcode", "returnType",
+                         "localVarsCount", "isInTryCatch", "isInFinalBlock", "className", "methodName",
+                         "blockNumber", "lineNumber", "numTests"]]
+            # Transform each column.. do id last since we need to inverse that.
+            for col in ["mutOperator", "returnType", "className", "methodName", "id"]:
+                data[col] = encoder.fit_transform(data[col])
 
-                clustering = AgglomerativeClustering(distance_threshold=None,
-                                                     n_clusters=int(math.ceil(len(data) * reduction)),
-                                                     linkage="ward",
-                                                     compute_distances=True)
-                clustering = clustering.fit(data)
+            clustering = AgglomerativeClustering(distance_threshold=None,
+                                                 n_clusters=int(math.ceil(len(data) * reduction)),
+                                                 linkage="ward",
+                                                 compute_distances=True)
+            clustering = clustering.fit(data)
 
-                # unlabel id so we can recognize the mutants
-                data["id"] = encoder.inverse_transform(data["id"])
-                export_clusters(clustering.labels_, data, csvPath)
+            # unlabel id so we can recognize the mutants
+            data["id"] = encoder.inverse_transform(data["id"])
+            export_clusters(clustering.labels_, data, csvPath)
 
-                results = calculate_clustered_score(csvPath, seed)
+            results = calculate_clustered_score(csvPath, seed)
 
-                results_df = results_df.append(
-                    {'project': project, 'reduction': reduction, 'score': results['score'],
-                     'acc_avg': results['acc_avg'],
-                     'acc_min': results['acc_min'], 'acc_max': results['acc_max']},
-                    ignore_index=True)
-                results_df.to_csv(directory + "/no-distance" + "/results_exp_no_similarity_" + str(seed) + ".csv",
-                                  sep=',', index=False)
+            results_df = results_df.append(
+                {'project': project, 'reduction': reduction, 'score': results['score'],
+                 'acc_avg': results['acc_avg'],
+                 'acc_min': results['acc_min'], 'acc_max': results['acc_max']},
+                ignore_index=True)
+            results_df.to_csv(directory + "/no-distance" + "/results_exp_no_similarity_" + str(seed) + ".csv",
+                              sep=',', index=False)
 
-        results_df.to_csv(directory + "/no-distance" + "/results_exp_no_similarity_" + str(seed) + ".csv", sep=',', index=False)
+    results_df.to_csv(directory + "/no-distance" + "/results_exp_no_similarity_" + str(seed) + ".csv", sep=',',
+                      index=False)
 
-    # plt.title('Hierarchical Clustering Dendrogram (truncated)')
-    # plt.xlabel('sample index or (cluster size)')
-    # plt.ylabel('distance')
-    # dendrogram(
-    #     calc_linkage_matrix(clustering),
-    #     truncate_mode='lastp',  # show only the last p merged clusters
-    #     p=12,  # show only the last p merged clusters
-    #     leaf_rotation=90.,
-    #     leaf_font_size=12.,
-    #     show_contracted=True,  # to get a distribution impression in truncated branches
-    # )
-    # plt.show()
+# plt.title('Hierarchical Clustering Dendrogram (truncated)')
+# plt.xlabel('sample index or (cluster size)')
+# plt.ylabel('distance')
+# dendrogram(
+#     calc_linkage_matrix(clustering),
+#     truncate_mode='lastp',  # show only the last p merged clusters
+#     p=12,  # show only the last p merged clusters
+#     leaf_rotation=90.,
+#     leaf_font_size=12.,
+#     show_contracted=True,  # to get a distribution impression in truncated branches
+# )
+# plt.show()
