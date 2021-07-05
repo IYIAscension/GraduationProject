3877a25911a8f7ad46f51111cad5945cff78d07b
Rasjaad <rasjaad@nivero.io>
2021-07-05 13:44:10 +0200

    updated seeds

--- hierarchical-clustering/main_full.py
+++ hierarchical-clustering/main_full.py
@@ -184,7 +184,7 @@ if __name__ == "__main__":
     projects1 = ['commons-text', 'commonc-codec', ]
     seeds = [
         66304, 16389, 14706, 91254, 49890, 86054, 55284, 77324, 36147, 13506, 73920, 80157, 43981, 75358, 33399, 56134,
-        13388, 81617, 90957, 52113, 20428, 26482, 56340, 31018, 32067, 13067, 8339, 49008, 14706, 68282, ]
+        13388, 81617, 90957, 52113, 20428, 26482, 56340, 31018, 32067, 13067, 8339, 49008, 125894, 68282, ]
     print(datetime.datetime.now())
     for project in projects:
         results_df = pandas.DataFrame(columns=['seed', 'reduction', 'score', 'acc_avg', 'acc_min', 'acc_max', ])
